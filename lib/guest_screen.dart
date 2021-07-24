7f96778090da11915fa747464adeb56703b6906d
Matthew Adian Hartono <68260159+MatthewADIAN@users.noreply.github.com>
2021-07-24 18:48:30 +0700

    Update guest_screen.dart

--- lib/guest_screen.dart
+++ lib/guest_screen.dart
@@ -1,12 +1,17 @@
 import 'dart:convert';
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:first_app/model/guest_data.dart';
 import 'package:first_app/second_screen.dart';
 import 'package:first_app/Album.dart';
 import 'package:http/http.dart' as http;
 
-class GuestScreen extends StatelessWidget {
+class GuestScreen extends StatefulWidget {
+  @override
+  _GuestScreenState createState() => _GuestScreenState();
+}
 
+class _GuestScreenState extends State<GuestScreen> {
   Future<Album> fetchAlbum() async {
     final response = await http
         .get(Uri.parse('http://www.mocky.io/v2/596dec7f0f000023032b8017'));
@@ -22,6 +27,14 @@ class GuestScreen extends StatelessWidget {
     }
   }
 
+  late Future<Album> futureAlbum;
+
+  @override
+  void initState() {
+    super.initState();
+    futureAlbum = fetchAlbum();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,25 +48,40 @@ class GuestScreen extends StatelessWidget {
           shrinkWrap: true,
           children: guestList.map((data) {
             return InkWell(
-              onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return SecondScreen(namaLomba: '', namaSecond: '',);
-                }));
-              },
-              child: Card(
-                child: Column(
-                  children: <Widget>[
-                    Expanded(
-                      child: Image.asset(
-                        data.imageAsset,
-                        fit: BoxFit.cover,
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return SecondScreen(
+                      namaLomba: '',
+                      namaSecond: '',
+                    );
+                  }));
+                },
+                child: Card(
+                  child: Column(
+                    children: <Widget>[
+                      Expanded(
+                        child: Image.asset(
+                          data.imageAsset,
+                          fit: BoxFit.cover,
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 8),
-                  ],
-                ),
-              ),
-            );
+                      SizedBox(height: 8),
+                      FutureBuilder<Album>(
+                        future: futureAlbum,
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            return Text(snapshot.data!.name);
+                          } else if (snapshot.hasError) {
+                            return Text('${snapshot.error}');
+                          }
+
+                          // By default, show a loading spinner.
+                          return const CircularProgressIndicator();
+                        },
+                      ),
+                    ],
+                  ),
+                ));
           }).toList(),
         ),
       ),
