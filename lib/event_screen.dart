5485b70b80f9e4a95837ca367dc70179e7804fa6
Matthew Adian Hartono <68260159+MatthewADIAN@users.noreply.github.com>
2021-07-26 08:52:22 +0700

    Update event_screen.dart

--- lib/event_screen.dart
+++ lib/event_screen.dart
@@ -13,103 +13,115 @@ class EventScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SingleChildScrollView(
-      physics: ScrollPhysics(),
-      child: SafeArea(
-        child: Column(
-          children: <Widget>[
-            Row(
+          physics: ScrollPhysics(),
+          child: SafeArea(
+            child: Column(
               children: <Widget>[
-                Expanded(
-                  flex: 1,
-                  child: Image.asset(
-                    'images/btn_backArticle_normal.png',
-                  ),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      flex: 1,
+                      child: Image.asset(
+                        'images/btn_backArticle_normal.png',
+                      ),
+                    ),
+                    SizedBox(
+                      width: 15.0,
+                    ),
+                    Expanded(
+                      child: Text('MESSAGE FROM $namaEvent'),
+                      flex: 4,
+                    ),
+                    SizedBox(
+                      width: 50.0,
+                    ),
+                    Expanded(
+                      child: Icon(Icons.search),
+                      flex: 1,
+                    ),
+                    InkWell(
+                      onTap: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                              return EventScreenFragment(
+                                namaEvent: this.namaEvent,
+                              );
+                            }));
+                      },
+                      child: Expanded(
+                        child: Image.asset(
+                            'images/btn_newMediaArticle_normal.png'),
+                        flex: 1,
+                      ),
+                    ),
+                  ],
                 ),
-                SizedBox(
-                  width: 15.0,
-                ),
-                Expanded(
-                  child: Text('MESSAGE FROM $namaEvent'),
-                  flex: 4,
-                ),
-                SizedBox(
-                  width: 50.0,
-                ),
-                Expanded(
-                  child: Icon(Icons.search),
-                  flex: 1,
-                ),
-                InkWell(
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) {
-                          return EventScreenFragment(
-                            namaEvent: this.namaEvent,
-                          );
-                        }));
+                ListView.builder(
+                  physics: NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    final EventData event = eventList[index];
+                    return InkWell(
+                      onTap: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                              return SecondScreen(
+                                namaSecond: namaEvent,
+                                namaLomba: event.nama,
+                                namaGuest: 'pilih guest',
+                                tglLahir: -1,
+                                bulanLahir: 0,
+                              );
+                            }));
+                      },
+                      child: Card(
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Expanded(
+                              flex: 2,
+                              child: Padding(
+                                padding: const EdgeInsets.all(12.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      event.nama,
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold
+                                      ),
+                                    ),
+                                    Text(
+                                      event.tanggal,
+                                      style: TextStyle(
+                                          color: Colors.amber,
+                                      ),
+                                    ),
+                                    Text(
+                                        event.tags
+                                    ),
+                                    SizedBox(height: 30.0,),
+                                    Text(
+                                        event.caption
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              flex: 1,
+                              child: Image.asset(event.imageAsset),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
                   },
-                  child: Expanded(
-                    child: Image.asset('images/btn_newMediaArticle_normal.png'),
-                    flex: 1,
-                  ),
+                  itemCount: eventList.length,
                 ),
               ],
             ),
-            ListView.builder(
-              physics: NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              itemBuilder: (context, index) {
-                final EventData event = eventList[index];
-                return InkWell(
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) {
-                      return SecondScreen(
-                        namaSecond: namaEvent,
-                        namaLomba: event.nama,
-                        namaGuest: 'pilih guest',
-                        tglLahir: -1,
-                        bulanLahir: 0,
-                      );
-                    }));
-                  },
-                  child: Card(
-                    child: Row(
-                      children: <Widget>[
-                        Expanded(
-                          flex: 2,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(
-                                event.nama
-                              ),
-                              Text(
-                                  event.tanggal
-                              ),
-                              Text(
-                                  event.tags
-                              ),
-                              Text(
-                                  event.caption
-                              ),
-                            ],
-                          ),
-                        ),
-                        Expanded(
-                          flex: 1,
-                          child: Image.asset(event.imageAsset),
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              },
-              itemCount: eventList.length,
-            ),
-          ],
-        ),
-      ),
-    ));
+          ),
+        ));
   }
 }
